9e56c8bc44bb6edfe2c5cbda5234074b1214be80
Fedor Kravchenko <f.kravchenko.ee@gmail.com>
2023-03-28 20:16:45 +0300

    draft

---
+++ src/components/Body.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import 'bulma/css/bulma.min.css';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+class Body extends React.Component<any, any>{
+    render() {
+        return (
+            <section className="hero is-fullheight">
+                <div className="hero-body">
+
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Body;
